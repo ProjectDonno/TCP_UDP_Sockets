ba6f543086442a3ebbe95e8e40d23eba3be5a899
Alexander <78038325+ProjectDonno@users.noreply.github.com>
2022-04-03 23:18:32 +0500

    Add files via upload

---
+++ ClientTcp/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            #region TCP
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Введите сообщение:");
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+
+            Console.ReadLine();
+            #endregion
+
+            /*
+            #region UDP
+            const string _ip = "127.0.0.1";
+            const int _port = 8082;
+
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(_ip), _port);
+
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+
+            while(true)
+            {
+                Console.WriteLine("Введите сообщение");
+                var _message = Console.ReadLine();
+                var serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081); 
+                udpSocket.SendTo(Encoding.UTF8.GetBytes(_message), serverEndPoint);
+
+                var _buffer = new byte[256];
+                var _size = 0;
+                var _data = new StringBuilder();
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
+
+                do
+                {
+
+                    _size = udpSocket.ReceiveFrom(_buffer, ref senderEndPoint);
+                    _data.Append(Encoding.UTF8.GetString(_buffer));
+                }
+                while (udpSocket.Available > 0);
+
+                Console.WriteLine(_data);
+                Console.ReadLine();
+            }
+            #endregion
+            */
+        }
+    }
+}
---
+++ ServerTcp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Project18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            #region TCP
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            // точка, к которой мы будем обращаться
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            // сокет, который мы будем использовать
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // связывает сокет с точкой
+            tcpSocket.Bind(tcpEndPoint);
+            // очередь "слушателей" состоит из 5 слотов
+            tcpSocket.Listen(5);
+
+            while(true)
+            {
+                // создаётся для каждого нового клиента
+                var listener = tcpSocket.Accept();
+                // место хранения полученных данных, максимум можем получить сообщение из 256 байт
+                var buffer = new byte[256];
+                // количество байт, реально полученных из сообщения
+                var size = 0;
+                var data = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (listener.Available > 0);
+
+                Console.WriteLine(data);
+
+                listener.Send(Encoding.UTF8.GetBytes("Успех"));
+
+                // выключили
+                listener.Shutdown(SocketShutdown.Both);
+                // закрыли
+                listener.Close();
+            }
+            #endregion
+
+            /*
+            #region UDP
+            const string _ip = "127.0.0.1";
+            const int _port = 8081;
+
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(_ip),_port);
+
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+            
+            while(true)
+            {
+
+            
+            var buffer = new byte[256];
+            var size = 0;
+            var data = new StringBuilder();
+            EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            do
+            {
+
+                size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
+                data.Append(Encoding.UTF8.GetString(buffer));
+            }
+            while (udpSocket.Available > 0);
+
+            udpSocket.SendTo(Encoding.UTF8.GetBytes("Сообщение получено"),senderEndPoint);
+
+            Console.WriteLine(data);
+            }
+            // TODO: закрытие сокета
+            #endregion
+            */
+        }
+    }
+}
